12ad148f3a6657a82fa3b14980255ac2223695eb
Werewulf <diskkitten@gmail.com>
2026-09-05 19:29:03 -0400

    Create Assistant.meta.js

---
+++ AA/Assistant.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Ascension Assistant
+// @namespace    https://example.local/
+// @version      3.6.8
+// @description  Multi-tab helper for Infinite Ascension
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_openInTab
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addValueChangeListener
+// @connect      raw.githubusercontent.com
+// @match        https://discord.com/*
+// @run-at       document-start
+// @downloadURL  https://raw.githubusercontent.com/WerewuIf/Ascension-Assistant/main/AA/Assistant.user.js
+// @updateURL    https://raw.githubusercontent.com/WerewuIf/Ascension-Assistant/main/AA/Assistant.meta.user.js
+// ==/UserScript==
